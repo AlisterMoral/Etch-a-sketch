d178aafed6b5a2f672b5f4b3ccc5f9a9ddf9c5d3
Alister <Alister.moral@gmail.com>
2023-04-26 10:12:32 +0200

    created a hover effect so the grid divs change color when mouse passes over

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 const container = document.createElement("div");
 container.classList.add("container");
+document.body.appendChild(container);
 
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement("div");
-  row.classList.add("row");
-  for (let j = 0; j < 16; j++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    row.appendChild(square);
+function createGrid(numSquares) {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
   }
-  container.appendChild(row);
+
+  for (let i = 0; i < numSquares; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < numSquares; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "black";
+      });
+      row.appendChild(square);
+    }
+    container.appendChild(row);
+  }
 }
 
-document.body.appendChild(container);
+createGrid(16); // default
+
+const resetBtn = document.getElementById("reset-btn");
+resetBtn.addEventListener("click", () => {
+  let numSquares = prompt("Enter the number of squares per side for the new grid (max: 100):");
+  if (numSquares === null) {
+    return;
+  }
+  numSquares = parseInt(numSquares);
+  if (isNaN(numSquares) || numSquares <= 0 || numSquares > 100) {
+    alert("Invalid input. Please enter a number between 1 and 100.");
+    return;
+  }
+  createGrid(numSquares);
+});
